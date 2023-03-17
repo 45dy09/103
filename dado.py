27acda1d9355a87b5420db3472acfc0266e994a4
45dy09 <92959976+45dy09@users.noreply.github.com>
2023-03-17 15:55:56 -0600

    Create dado.py

---
+++ dado.py
@@ -0,0 +1,30 @@
+import random
+response="x"
+while response=="x":
+    n=random.randint (1,6)
+    if n==1:
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+    if n==2:
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+    if n==3:
+        print("[0    ]")
+        print("[  0  ]")
+        print("[    0]")
+    if n==4:
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+    if n==5:
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+    if n==6:
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+    response=input("presiona x para girar ")
+    print("\n")
